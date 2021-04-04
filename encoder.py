17854db87fe49af96614b4ff9b1fa31c607cbc5b
Praneeth reddy Kunduru <praneethreddykunduru@Praneeths-MacBook-Air.local>
2021-04-03 23:58:50 -0700

    encoder layer

---
+++ encoder.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+from tensorflow import keras
+import attention.py
+class feed_forward(keras.Model):
+	def __init__(self,max_seq_len,d_model):
+		super(feed_forward).__init__()
+		self.d_model=d_model
+		self.max_seq_len=max_seq_len
+		self.weights=self.add_weight(
+			name='final_weights',
+			shape=(d_model,d_model)
+			initializer='random_normal',
+			trainable=True
+			)
+		self.bias=self.add_weight(
+			name='bias',
+			shape=(max_seq_len,d_model),
+			initializer='random_normal',
+			trainable=True
+			)
+	def call(self,data):
+		return tf.matmul(data,weights)+bias
+
+class encoder(keras.Model):
+	def __init__(self,number_heads,max_seq_len,d_model,expected_len):
+		super(encoder).__init__()
+		self.number_heads=number_heads
+		self.max_seq_len=max_seq_len
+		self.d_model=d_model
+		self.expected_len=expected_len
+		self.mha=MultiHeadAttention(number_heads=number_heads,max_seq_len=max_seq_len,d_model=d_model,expected_len=expected_len)
+		self.normalise1=tf.keras.layers.LayerNormalisation(epsilon=1e-6)
+		self.normalise2=tf.keras.layers.LayerNormalisation(epsilon=1e-6)
+		self.drop1=tf.keras.layers.Dropout(0.1)
+		self.forward=feed_forward(max_seq_len,d_model)
+	
+	def call(self,data):
+		attention_output=self.mha(data)
+		attention_output=self.drop1(attention_output,training=training)
+		first_out=self.normalise1(attention_output+data)
+		first_out1=self.feed_forward(first_out)
+		out=self.normalise2(first_out1+first_out)
+		return out
+
+
